fbc1195183e53ba32ae51b933e005e44f2f7fc38
steve <mariscurrenalae@gmail.com>
2026-05-11 14:47:37 -0600

    test: All test files

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "snyk-test-cicd-2026"
+}
+
+# Vulnerability: Bucket with public ACL
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.test_bucket.id
+  acl    = "public-read" 
+}
+
+# Vulnerability: No server cipher
